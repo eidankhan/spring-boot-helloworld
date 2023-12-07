c05f56ebf02faa131a336f80483f823dedc29be5
eidankhan <eidan.khan659@gmail.com>
2023-12-07 00:25:04 +0000

    first commit

---
+++ src/main/java/com/example/springboothelloworld/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World! Your application is running";
+    }
+}
